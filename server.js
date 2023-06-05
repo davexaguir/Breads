ecb98e2c085cc1a87cbdf8a4aa29facb18c02aef
dagui <davaguir852@gmail.com>
2023-06-04 22:33:10 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -8,9 +8,13 @@ console.log(PORT)
 const app = express()
 
 //Middleware
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+
+
+//ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Bread!')
 })
@@ -18,6 +22,12 @@ app.get('/', (req, res) => {
 //Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+  
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
